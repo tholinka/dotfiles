36f8a2ea1a3bfee0a967cdf0413f2d5aa34cb8d1
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2017-10-14 11:31:54 -0600

    Change default git settings

--- Settings/setup.sh
+++ Settings/setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-echo "RUN THIS FROM THE DIRECTORY IT IS IN, OR THINGS WILL BREAK"
-
-# make a bunch of symlinks
-
-
-
+SCRIPTLOC=$(readlink -f "$0")
+FOLDERLOC=$(dirname "$SCRIPTLOC")
 ### .config section ###
 if [ ! -d ~/.config ]; then
     mkdir ~/.config
@@ -79,39 +75,46 @@ fi
 
 ## symlinks
 ### .config section ###
-ln -sf "$PWD/config/Code/User/keybinds.json" ~/.config/Code/User/
-ln -sf "$PWD/config/Code/User/settings.json" ~/.config/Code/User/
+ln -sf "$FOLDERLOC/config/Code/User/keybinds.json" ~/.config/Code/User/
+ln -sf "$FOLDERLOC/config/Code/User/settings.json" ~/.config/Code/User/
 
-ln -sf "$PWD/config/QtProject/qtcreator/styles/monokai_tyler.xml" ~/.config/QtProject/qtcreator/styles/
+ln -sf "$FOLDERLOC/config/QtProject/qtcreator/styles/monokai_tyler.xml" ~/.config/QtProject/qtcreator/styles/
 
-ln -sf "$PWD/config/zathura/zathurarc" ~/.config/zathura/
+ln -sf "$FOLDERLOC/config/zathura/zathurarc" ~/.config/zathura/
 
-ln -sf "$PWD/vim/colors/monokai.vim" ~/.config/nvim/colors
-ln -sf "$PWD/vim/vim-runtime/" ~/.config/nvim/vim-runtime
-ln -sf "$PWD/vim/vimrc" ~/.config/nvim/init.vim
+ln -sf "$FOLDERLOC/vim/colors/monokai.vim" ~/.config/nvim/colors
+ln -sf "$FOLDERLOC/vim/vim-runtime/" ~/.config/nvim/vim-runtime
+ln -sf "$FOLDERLOC/vim/vimrc" ~/.config/nvim/init.vim
 
 # link both chrome and chromium against flags, these might be an arch only things ?
-ln -sf "$PWD/config/chromium-flags.conf" ~/.config/chromium-flags.conf
-ln -sf "$PWD/config/chromium-flags.conf" ~/.config/chrome-flags.conf
+ln -sf "$FOLDERLOC/config/chromium-flags.conf" ~/.config/chromium-flags.conf
+ln -sf "$FOLDERLOC/config/chromium-flags.conf" ~/.config/chrome-flags.conf
 ### end of .config ###
 ### .local section ###
-ln -sf "$PWD/local/share/konsole/monokai.colorscheme" ~/.local/share/konsole/
-ln -sf "$PWD/local/share/konsole/Shell.profile" ~/.local/share/konsole/Shell.profile
+ln -sf "$FOLDERLOC/local/share/konsole/monokai.colorscheme" ~/.local/share/konsole/
+ln -sf "$FOLDERLOC/local/share/konsole/Shell.profile" ~/.local/share/konsole/Shell.profile
 
 ## have to copy, steam can't do symlinks for skins
-cp -r "$PWD/local/share/Steam/skins/Metro 4.2.4" ~/.steam/steam/skins/
+cp -r "$FOLDERLOC/local/share/Steam/skins/Metro 4.2.4" ~/.steam/steam/skins/
 ### end of .local ###
 ### couple random folders ###
-ln -sf "$PWD/vim" ~/.vim
+ln -sf "$FOLDERLOC/vim" ~/.vim
 # replace ~/.vimrc with a link to ~/.vim
 echo "source ~/.vim/vimrc" > ~/.vimrc
 # pull in vim runtime link vim-runtime as it expects
-ln -sf "$PWD/vim/vim-runtime" ~/.vim_runtime
+ln -sf "$FOLDERLOC/vim/vim-runtime" ~/.vim_runtime
 
-ln -sf "$PWD/vscode/settings.json" ~/.vscode/
+ln -sf "$FOLDERLOC/vscode/settings.json" ~/.vscode/
 
-ln -sf "$PWD/astylerc" ~/.astylerc
-ln -sf "$PWD/gitgeneralconfig" ~/.gitgeneralconfig
+ln -sf "$FOLDERLOC/astylerc" ~/.astylerc
+
+ln -sf "$FOLDERLOC/gitgeneralconfig" ~/.gitgeneralconfig
+
+# copy editorconfig, gitattributes, and gitignore
+cp $FOLDERLOC/editorconfig ~/.editorconfig
+cp $FOLDERLOC/gitattributes ~/.gitattributes
+cp $FOLDERLOC/gitignore ~/.gitignore
+
 echo "[include]
     path = ~/.gitgeneralconfig" >> ~/.gitconfig
 echo "Included link to .gitgeneralconfig in ~/.gitconfig, make sure there aren't to many links of that in there"
