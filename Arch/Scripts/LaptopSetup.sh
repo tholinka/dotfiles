4ad3a1c1beb62cf7bdc2b631b28a97975c039074
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2017-10-18 09:12:12 -0600

    Add laptopsetup script

---
+++ Arch/Scripts/LaptopSetup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+source includes/colordefines.sh
+
+if ! type pacaur &>/dev/null; then
+    echo -e "${GB}NO PACAUR, use \"AurSetup\" script first! ${RESET}"
+    return;
+fi
+
+echo -e "${GB}Installing laptop-mode-tools + optional deps ${RESET}"
+
+pacaur -S --needed --noconfirm laptop-mode-tools
+sudo pacman -S --asdeps --needed --noconfirm acpid bluez-utils hdparm sdparm ethtool wireless_tools xorg-xset python2-pyside
+
+echo -e "${GB}Enabling laptop mode service in systemctl ${RESET}"
+sudo systemctl enable laptop-mode.service
+
+echo -e "${CB}Done, edit /etc/laptop-mode/laptop-mode.conf and /etc/laptop-mode/conf.d/* to liking ${RESET}"
+echo -e "${CYAN} of note is /etc/laptop-mode/conf.d/auto-hibernate.conf, which can be enabled by setting ENABLE_AUTO_HIBERNATE=1 ${RESET}"
