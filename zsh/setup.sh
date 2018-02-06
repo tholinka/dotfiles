75faa26274c4e8883dec3a0af9f4962d23055842
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2018-02-06 02:05:31 +0000

    Update zsh setup to tell user what it's doing

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,16 +2,21 @@ SCRIPTLOC=$(readlink -f "$0")
 FOLDERLOC=$(dirname "$SCRIPTLOC")
 # create symblink to config-files and ~/.zsh-config
 ## remove existing one so that a symlink isn't created at ~/.zsh-config/.zsh-config
+
 if [ -e ~/.zsh-config ]; then
-    rm ~/.zsh-config # should be a symlink
+   echo "Removing ~/.zsh-config symlink"
+   rm ~/.zsh-config # should be a symlink
 fi
 
+echo "Creating ~/.zsh-config symlink"
 ln -s "$FOLDERLOC/config-files" ~/.zsh-config
 
 # replace ~/.zshrc with a pointer here
+echo "Replacing ~/.zshrc with a pointer to ~/.zsh-config/zshrc.zshrc"
 echo "source ~/.zsh-config/zshrc.zshrc" > ~/.zshrc
 
 # replace ~/.zprofile with a pointer here
+echo "Replacing ~/.zprofile with a pointer to ~/.zsh-config/zprofile.zshrc"
 echo "source ~/.zsh-config/zprofile.zshrc" > ~/.zprofile
 
 # if no pacman remind to install powerline fonts for theme
