0956068679f9ab22f26e53bb3973b116a28a6e0a
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2018-05-30 05:09:10 -0600

    Remove maximum-cache-ttl from gpg config as it caused issues on a pi2
    also add note about restarting gpg after setup file

--- Settings/setup.sh
+++ Settings/setup.sh
@@ -122,4 +122,7 @@ echo "[include]
     path = ~/.gitgeneralconfig" >> ~/.gitconfig
 echo "Included link to .gitgeneralconfig in ~/.gitconfig, make sure there aren't to many links of that in there"
 
+echo;
+
 echo "Also tell git about your gpg key: \"gpg --list-secret-keys --keyid-format LONG\" to get the key and \"git config --global user.signingkey [key from previous command]\" to have git use it"
+echo "You may also need to restart the agent (probably not, but maybe): \"gpg--connect-agent reloadagent /bye\""
