ba34fea9e1178b34dbb7e660e0a757e3fb565186
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2018-06-08 02:27:46 -0600

    Check for git version in update script
    Some distros (ubuntu subsystem for windows) are to old for git to have --jobs

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 SCRIPTLOC=$(readlink -f "$0")
 FOLDERLOC=$(dirname "$SCRIPTLOC")
 
@@ -17,7 +16,15 @@ if [ ${#files[@]} -ne 0 ]; then
     done
 fi
 
-# this is aliased to gitupdatesubmodules in my zsh settings
-git submodule update --jobs 4 --recursive --remote
+# see if update can be done in parellel (--jobs) (added in git version 2.9.0)
 
-cd -
+if [ $(printf "$(git --version | sed "s/git version //")\n2.9.0" | sort -V | head -n1) = "2.9.0" ]; then
+    # 2.9.0 is a newer string than git --version, so it should have it
+    GIT_JOBS="--jobs $(nproc --all)"
+fi
+
+# this is aliased to gitupdatesubmodules in my zsh settings
+# don't quote GIT_JOBS, we want to to split on the space
+git submodule update $GIT_JOBS --recursive --remote
+
+cd - 1>/dev/null
