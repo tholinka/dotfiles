e4bf3cebbc5bcaf7dd0d31d566d9234b60a3a21c
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2019-02-10 21:14:43 -0700

    Add update git sh

---
+++ update-git.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# this script rewrites git commits email and author name to new email and author name
+# edit "old@email" to the email address you want to change, and "new@email" and "new name"
+# to the new email / name you want the commits to have
+# then force push, after checking that the commits are all still good (they should be, but double check!)
+
+echo "Edit the email and author name sections before running"
+exit 1
+
+# pull changes that might have happened
+git pull
+
+# replace link123451-?noreply
+git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "old@email"  ]; then
+GIT_AUTHOR_EMAIL="new@email";
+GIT_AUTHOR_NAME="new name";
+GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL";
+GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
+
+# drop originals from before change
+git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
+
+echo;
+echo "Run git push --force if everything is good"
